7c6734d982237e39fc593206c6798ba5f972a0a4
Ivo Georgiev <ivogeorg@gmail.com>
2019-10-17 12:51:32 -0600

    Create negate_program.c

---
+++ negate_program.c
@@ -0,0 +1,17 @@
+// A function that returns its
+// argument negated
+int negate(int a) {
+    return (-1) * a;
+}
+
+// Every program in the C language always starts in the 'main()' function
+int main() {
+    int value = 6;
+
+    // Calling another function will
+    // cause a new stack frame to be
+    // set up for it
+    value = negate(value);
+
+    return 0;
+}
